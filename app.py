b38240a574efa758e500f12df4f99953d7425e19
WangXing117 <123046757@upq.edu.mx>
2025-06-22 13:25:09 -0600

    Inserts de medicos
    Con la capacidad de insertar médicos a la base de datos

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from connection import getConnection
 from flask import Flask, jsonify, render_template, request, url_for, flash, redirect, session
-from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = "mysecretkey"
 
 # Funciones
-def connectDB(index):
-    return getConnection(index).cursor()
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+
+def encriptar_contrasena(password_plano):
+   
+    # Genera el hash en formato UTF-8 (string)
+    hash_utf8 = bcrypt.generate_password_hash(password_plano).decode('utf-8')
+    
+    # Convierte el string a bytes para almacenar como VARBINARY
+    hash_bytes = hash_utf8.encode('utf-8')
+    
+    return hash_bytes
+
 
 def validarSesion(valor, redirigir):
     if valor not in session:
@@ -16,35 +28,67 @@ def validarSesion(valor, redirigir):
 #Rutas
 @app.route("/")
 def home():
-    return render_template("POOproyecto/login.html")
+    return render_template("AgregarMedico.html")
 
-@app.route("/medico")
-def medico():
-    nombre = session.get("nombre_medico", "Invitado")
-    return validarSesion("nombre_medico", "login") or render_template("POOproyecto/Medico.html", nombre_medico = nombre)
+# Manejo de medicos
+@app.route("/agregar_medico", methods = ["POST"])
+def agregarMedico():
+    errores = {}
+    nombre = request.form.get("nombre", "").strip()
+    apellido_paterno = request.form.get("apellido_P", "").strip()
+    apellido_materno = request.form.get("apellido_M", "").strip()
+    cedula = request.form.get("cedula", "").strip()
+    rfc = request.form.get("rfc", "").strip()
+    correo = request.form.get("correo", "").strip()
+    passwordNE = request.form.get("password", "").strip()
+    id_rol = request.form.get("rol", "").strip()
 
-@app.route("/medicoAdmin")
-def medicoAdmin():
-    nombre = session.get("nombre_medico", "Invitado")
-    return validarSesion("nombre_medico", "login") or render_template("POOproyecto/MedicoAdmin.html", nombre_medico = nombre)
+    if not rfc or not nombre or not apellido_paterno or not apellido_materno or not passwordNE or not id_rol or not cedula:
+        errores["emptyValues"] = "Valores vacíos"
 
-@app.route("/cita")
-def cita():
-    return render_template("POOproyecto/AgregarCita.html")
+    if len(rfc) != 13:
+        errores["rfcLen"] = "El RFC debe tener 13 caracteres"
+    
+    if len(passwordNE) < 8:
+        errores["passwordLen"] = "La contraseña debe tener al menos 8 caracteres"
 
-@app.route("/expediente")
-def expediente():
-    return render_template("POOproyecto/AgregarExp.html")
+    if len(cedula) < 7:
+        errores["cedulaLen"] = "La cédula profesional debe tener al menos 7 caracteres"
 
-@app.route("/consulta")
-def consulta():
-    return render_template("POOproyecto/consulta.html")
+    if not errores:
+        try:
+           
+            hashed_bytes = encriptar_contrasena(passwordNE)
 
-@app.route("/consultarExpediente")
-def consutar_expediente():
-    return render_template("POOproyecto/ConsultarExp.html")
+            conn = getConnection(2)
+            cursor = conn.cursor()
 
+            cursor.execute("""
+                            DECLARE @res INT;
+                            EXEC InsertarMedico ?, ?, ?, ?, ?, ?, ?, ?, @res OUTPUT;
+                            SELECT @res AS Resultado;
+                        """, nombre, apellido_paterno, apellido_materno, cedula, rfc, correo, hashed_bytes, id_rol)
 
+            resultado = cursor.fetchone()
+
+            if resultado and resultado.Resultado == 1:
+                errores["rfcExists"] = "El RFC ya está registrado"
+                return render_template("AgregarMedico.html", errores=errores)
+            
+            if resultado and resultado.Resultado == 2:
+                errores["mailExists"] = "El correo ya está registrado"
+                return render_template("AgregarMedico.html", errores=errores)
+            
+            conn.commit()
+            flash("Médico agregado exitosamente")
+
+        except Exception as e:
+            errores["dbError"] = "Error al agregar médico"
+            print(f"Error: {e}")
+        finally:
+            cursor.close()
+
+    return render_template("AgregarMedico.html", errores = errores)
 
 @app.route("/login", methods = ["POST"])
 def login():
@@ -58,7 +102,8 @@ def login():
 
     if not errors:
         try:
-            cursor = connectDB(1)
+            conn = getConnection(2)
+            cursor = conn.cursor()
             cursor.execute("SELECT CONCAT(Nombres, ' ', Apellido_paterno, ' ', Apellido_materno), Contrasena, ID_rol from Medicos WHERE rfc = ?", (rfc,))
             # cursor.execute("SELECT R.Nombre from Medicos M INNER JOIN Roles R ON M.ID_rol = R.ID_rol WHERE M.RFC = ?", (rfc,))
             # rol_tuple = cursor.fetchone()
@@ -88,7 +133,8 @@ def login():
 @app.route("/DBCheck")
 def dbCheck():
     try:
-        cursor = connectDB(2)  # Cambia el índice según la conexión que quieras probar
+        conn = getConnection(2)
+        cursor = conn.cursor()
         cursor.execute("Select 1")
         return jsonify({"status": "Ok", "message": "Conectado"}), 200
     except Exception as e:
