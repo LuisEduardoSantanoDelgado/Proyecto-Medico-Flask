d9199b4d7b16337d857f738c273bdaba3a72b0dd
WangXing117 <123046757@upq.edu.mx>
2025-07-15 19:59:26 -0600

    ayuda
    no se q hice

--- rutas/citas/agregarCita.py
+++ rutas/citas/agregarCita.py
@@ -1,12 +1,27 @@
-from flask import Blueprint, render_template , request, flash
+from flask import Blueprint, render_template , request, flash, session
 from BDAyudas.QueryExecute import execute_query
 from decorators.loginRequired import login_required
 from datetime import datetime
 
 agregarCita_bp = Blueprint('agregarCita', __name__)
-@agregarCita_bp.route("/agregarCita")
+@agregarCita_bp.route("/agregarCita/<id_paciente>")
 @login_required
-def mostrarAgregarCita():
+def mostrarAgregarCita(id_paciente):
+    errores={}
+    print('Entrando a agregar cita -------------------------------')
+    print(f'ID de paciente: {id_paciente} tipo: {type(id_paciente)}')
+    try:
+        if not id_paciente:
+            print(f"Campo vacio id paciente: {id_paciente}")
+            errores["dbError"] = "Error al obtener datos."
+        else:
+            nombrePaciente = execute_query("SELECT CONCAT(Nombres, ' ', Apellido_paterno, ' ', Apellido_materno) FROM Pacientes WHERE ID_pacientes = (?)", id_paciente, fetch="one")
+
+            return render_template('Citas/AgregarCita.html', nombrePaciente = nombrePaciente, paciente = id_paciente)
+    except Exception as e:
+        print(f'Ha ocurrido un error al intentar obtener datos del medico o del paciente: {str(e)}')
+        errores["dbError"] = "Error durante la obtencion de datos"
+        
     return render_template("Citas/AgregarCita.html")
 
 @agregarCita_bp.route("/agregarCita/continuar")
