ef92ee5a648bfdc8639d104f69b620ba9c55f10d
WangXing117 <123046757@upq.edu.mx>
2025-07-16 22:14:30 -0600

    b
    b

--- rutas/Citas/eliminarCita.py
+++ rutas/Citas/eliminarCita.py
@@ -4,7 +4,7 @@ from decorators.loginRequired import login_required
 from datetime import datetime
 
 eliminarCita_bp = Blueprint('eliminarCita', __name__)
-@eliminarCita_bp.route("/eliminarCita")
+@eliminarCita_bp.route("/eliminarCita/<id_cita>")
 @login_required
-def mostrarEliminarCita():
+def mostrarEliminarCita(id_cita):
     return render_template("Citas/EliminarCita.html")
--- rutas/VistasPrincipales/citasLista.py
+++ rutas/VistasPrincipales/citasLista.py
@@ -7,4 +7,28 @@ citasLista_bp = Blueprint('citasLista', __name__)
 
 @login_required
 def citasLista():
-    return render_template("VistasPrincipales/CitasLista.html")
+    print('Entrando a la lista de citas')
+    errores = {}
+    rfc = session.get("rfc")
+    try:
+        id_medico   = execute_query("SELECT dbo.IDMedico(?)", (rfc,), fetch="one")
+        print(f'ID medico llegado: {id_medico}')
+        if id_medico:
+            idMedico = id_medico[0]
+            tblCitas = execute_query(" SELECT Citas.ID_cita, CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) AS Nombre_Paciente FROM Citas JOIN Pacientes ON Citas.ID_paciente = Pacientes.ID_paciente WHERE Citas.ID_medico = (?) AND Citas.Estatus = (?)",(idMedico,1), fetch="all")
+            # tblCitas = execute_query("SELECT * FROM Citas WHRERE ID_medico = (?) and Estatus = (?)",(idMedico,1), fetch="all")
+            print(f'Tabla de citas: {tblCitas}')
+            if not tblCitas or len(tblCitas) == 0:
+                print('No hay citas')
+                errores["citasNotFound"] = "No se encontraron citas para mostrar"
+            else:
+                print('Enviando tabla de citas---->')
+                return render_template('VistasPrincipales/citasLista.html', tblCitas = tblCitas, errores = errores)
+        else:
+            print('No llego un ID de medico')
+            errores['medicoNotFound'] = "No se  hayo a un médico"
+    except Exception as e:
+        print(f'Error durante obtencion de lista de citas: {str(e)}')
+        errores['dbError'] = "Error durante obtencion de lista de citas"
+
+    return render_template("VistasPrincipales/CitasLista.html", errores = errores, tblCitas = [])
