4bf9101610815dafb3a6585e7d73d0e4e2235d87
LuisEduardoSantanoDelgado <123046272@upq.edu.mx>
2025-07-23 14:46:27 -0600

    Update citasLista.py

--- rutas/VistasPrincipales/citasLista.py
+++ rutas/VistasPrincipales/citasLista.py
@@ -1,34 +1,42 @@
-from flask import Blueprint, render_template , session
+from flask import Blueprint, render_template, request, session
 from BDAyudas.QueryExecute import execute_query
 from decorators.loginRequired import login_required
 
 citasLista_bp = Blueprint('citasLista', __name__)
-@citasLista_bp.route("/citasLista")
 
+@citasLista_bp.route("/citasLista", methods=["GET", "POST"])
 @login_required
 def citasLista():
     print('Entrando a la lista de citas ----------------------------')
     errores = {}
+    filtro = request.form.get('filtro')
+    busqueda = request.form.get('busqueda')
     rfc = session.get("rfc")
+    
     try:
-        id_medico   = execute_query("SELECT dbo.IDMedico(?)", (rfc,), fetch="one")
+        id_medico = execute_query("SELECT dbo.IDMedico(?)", (rfc,), fetch="one")
         print(f'ID medico llegado: {id_medico}')
+        
         if id_medico:
             idMedico = id_medico[0]
-            tblCitas = execute_query(" SELECT Citas.ID_cita, CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) AS Nombre_Paciente FROM Citas JOIN Pacientes ON Citas.ID_paciente = Pacientes.ID_paciente WHERE Citas.ID_medico = (?) AND Citas.Estatus = (?)",(idMedico,1), fetch="all")
-            print(f'Tabla de citas: {tblCitas}')
+            
+            if filtro == "Nombre":
+                query = "SELECT Citas.ID_cita, CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) AS Nombre_Paciente FROM Citas JOIN Pacientes ON Citas.ID_paciente = Pacientes.ID_paciente WHERE Citas.ID_medico = (?) AND Citas.Estatus = (?) AND CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) LIKE ?"
+                tblCitas = execute_query(query, (idMedico, 1, '%' + busqueda + '%'), fetch="all")
+            elif filtro == "Fecha":
+                query = "SELECT Citas.ID_cita, CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) AS Nombre_Paciente FROM Citas JOIN Pacientes ON Citas.ID_paciente = Pacientes.ID_paciente WHERE Citas.ID_medico = (?) AND Citas.Estatus = (?) AND Citas.Fecha LIKE ?"
+                tblCitas = execute_query(query, (idMedico, 1, '%' + busqueda + '%'), fetch="all")
+            else:
+                query = "SELECT Citas.ID_cita, CONCAT(Pacientes.Nombres, ' ', Pacientes.Apellido_paterno, ' ', Pacientes.Apellido_materno) AS Nombre_Paciente FROM Citas JOIN Pacientes ON Citas.ID_paciente = Pacientes.ID_paciente WHERE Citas.ID_medico = (?) AND Citas.Estatus = (?)"
+                tblCitas = execute_query(query, (idMedico, 1), fetch="all")
             
             if not tblCitas or len(tblCitas) == 0:
-                print('No hay citas')
                 errores["citasNotFound"] = "No se encontraron citas para mostrar"
-            else:
-                print('Enviando tabla de citas---->')
-                return render_template('VistasPrincipales/citasLista.html', tblCitas = tblCitas, errores = errores)
         else:
-            print('No llego un ID de medico')
-            errores['medicoNotFound'] = "No se  hayo a un médico"
+            errores['medicoNotFound'] = "No se encontró un médico"
+    
     except Exception as e:
-        print(f'Error durante obtencion de lista de citas: {str(e)}')
-        errores['dbError'] = "Error durante obtencion de lista de citas"
+        print(f'Error durante obtención de lista de citas: {str(e)}')
+        errores['dbError'] = "Error durante obtención de lista de citas"
 
-    return render_template("VistasPrincipales/CitasLista.html", errores = errores, tblCitas = [])
+    return render_template("VistasPrincipales/citasLista.html", errores=errores, tblCitas=tblCitas)
